bf64a68b52c25fab869685467559d0c110585bb3
Soham Chavan <sohamchavan159@gmail.com>
2025-06-27 12:23:24 +0530

    Client Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="Font-bold text-8xl text-red-600">Hello</h1>
+    </>
+  );
+}
+
+export default App;
